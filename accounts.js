d4ba5b8ff2382ba85d7f88a78b2c88625df2a678
Amit Kumar <amitkcodes@gmail.com>
2025-01-27 19:46:47 +0530

    added a function of fetching the balance

--- accounts.js
+++ accounts.js
@@ -1,8 +1,22 @@
-const {config: dotEnvConfig} = require('dotenv')
+const { config: dotEnvConfig } = require("dotenv");
 dotEnvConfig();
 
-const {ethers} = require('ethers')
-
+const { ethers } = require("ethers");
 
 const RPC_ENDPOINT = process.env.INFURA_SEPOLIA_RPC_URL;
-const ACCOUNT_PUBLIC_ADDRESS = process.env.METAMASK_PRIVATE_KEY;
+const ACCOUNT_PUBLIC_ADDRESS = process.env.ACCOUNT_PUBLIC_ADDRESS;
+
+const provider = new ethers.JsonRpcProvider(RPC_ENDPOINT);
+
+async function call() {
+  const balanceInWei = await provider.getBalance(ACCOUNT_PUBLIC_ADDRESS);
+  const balanceInEth = Number(ethers.formatEther(balanceInWei)).toFixed(5);
+  console.log({ balance: balanceInEth });
+}
+
+call()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error({ error: err });
+    process.exit(1);
+  });
